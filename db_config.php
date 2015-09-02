0ad85a02ba9fe0769c2b0cee6c2bcec3af2aebd0
Steve <sadennett@ieee.org>
2015-09-02 14:35:58 -0500

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
 
-define('DB_USER', "Steve"); // db user
-define('DB_PASSWORD', "1234"); // db password (mention your db password here)
+define('DB_USER', ""); // db user
+define('DB_PASSWORD', ""); // db password (mention your db password here)
 define('DB_DATABASE', "ims"); // database name
 define('DB_SERVER', "localhost"); // db server
 ?>
